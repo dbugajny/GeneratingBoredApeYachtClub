8fe66e0cade06105d176adeb83cd6bb20266764a
dbugajny <dawid.bugajny@gmail.com>
2023-05-25 19:20:23 +0200

    New type of model

--- src/models/classifier.py
+++ src/models/classifier.py
@@ -40,3 +40,44 @@ def build_classifier(
         outputs.append(tf.keras.layers.Dense(n, activation="sigmoid", name=feature_name)(x))
 
     return tf.keras.Model(inp, outputs, name="classifier")
+
+
+def build_classifier2(encoder_model: tf.keras.Model) -> tf.keras.Model:
+    for i in range(len(encoder_model.layers)):
+        encoder_model.layers[i].trainable = False
+
+    inp = tf.keras.layers.Input((256, 256, 3))
+    enc = encoder_model(inp)
+    concat = tf.keras.layers.Concatenate()([enc[0], enc[1], enc[2]])
+
+    # Mouth
+    m = DenseBlock(256, 0.2)(concat)
+    m = DenseBlock(256, 0.2)(m)
+    out_mouth = tf.keras.layers.Dense(33, activation="sigmoid", name="Mouth")(m)
+
+    # Background
+    out_background = tf.keras.layers.Dense(8, activation="sigmoid", name="Background")(concat)
+
+    # Hat
+    h = DenseBlock(256, 0.2)(concat)
+    h = DenseBlock(256, 0.2)(h)
+    out_hat = tf.keras.layers.Dense(37, activation="sigmoid", name="Hat")(h)
+
+    # Eyes
+    e = DenseBlock(256, 0.2)(concat)
+    e = DenseBlock(256, 0.2)(e)
+    out_eyes = tf.keras.layers.Dense(23, activation="sigmoid", name="Eyes")(e)
+
+    # Clothes
+    c = DenseBlock(256, 0.2)(concat)
+    c = DenseBlock(256, 0.2)(c)
+    out_clothes = tf.keras.layers.Dense(44, activation="sigmoid", name="Clothes")(c)
+
+    # Fur
+    f = DenseBlock(256, 0.2)(concat)
+    f = DenseBlock(256, 0.2)(f)
+    out_fur = tf.keras.layers.Dense(19, activation="sigmoid", name="Fur")(c)
+
+    outputs = [out_mouth, out_background, out_hat, out_eyes, out_clothes, out_fur]
+
+    return tf.keras.Model(inp, outputs, name="classifier")
